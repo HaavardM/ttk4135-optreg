bc854150c3084c1a30070509e4f741cdf0d6c59e
EdvardGrodem <edvard.grodem@gmail.com>
2019-02-14 09:53:09 +0100

    Added comments

--- MatlabFiles/ex3_matlab_files/ex3_init.m
+++ MatlabFiles/ex3_matlab_files/ex3_init.m
@@ -1,13 +1,12 @@
+%% Exercise 10.3
 addpath('../ex2_matlab_files');
-run ex_2
+run ex_2 %Find optimal path and input
 
-Q_lqr = diag([1 1 0 0]);
-R_lqr = diag(1);
+Q_lqr = diag([1 1 0 0]); %State weight
+R_lqr = diag(1);         %Input weight
 
-[K,S,E] = dlqr(A1,B1,Q_lqr,R_lqr);
+%Calculate discret LQR
+[K,S,E] = dlqr(A1,B1,Q_lqr,R_lqr); 
 
-
-
-figure;
-
+figure();
 f = load('ex3.mat');
